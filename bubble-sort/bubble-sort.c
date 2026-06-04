e28078773ee5978a5a25ae4e6d6350966e34e942
od86 <178620777+od86@users.noreply.github.com>
2026-06-04 21:12:03 +0100

    Created bubble sort sorting algorithm

---
+++ bubble-sort/bubble-sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Never have to loop through more than n-1 times
+int main() {
+  int unsortedArr[] = {5, 4, 3, 2, 1};
+
+  for (int i = 0; i < (sizeof(unsortedArr) / sizeof(unsortedArr[0])) - 1; i++)
+  {
+    for (int x = 0; x < (sizeof(unsortedArr) / sizeof(unsortedArr[0])) - 2; x++)
+    {
+      if (unsortedArr[x] > unsortedArr[x + 1]) {
+        int greater = unsortedArr[x];
+        unsortedArr[x] = unsortedArr[x + 1];
+        unsortedArr[x + 1] = greater;
+      }
+    }
+  }
+
+  for (int i = 0; i < (sizeof(unsortedArr) / sizeof(unsortedArr[0])) - 1; i++)
+  {
+    printf("%i ", unsortedArr[i]);
+  }
+
+  printf("\n");
+}
