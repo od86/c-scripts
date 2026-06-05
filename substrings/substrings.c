72135b059984e5ce515efa14cd3150e1eaafb4f6
od86 <178620777+od86@users.noreply.github.com>
2026-06-05 16:26:55 +0100

    Attempted to make substring method, does NOT work

---
+++ substrings/substrings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// 2d Array of words = {{'h', 'e', 'l', 'l', 'o'}, {'w', 'o', 'r', 'l', 'd'}}
+// 1d Array of ints for number of times each word has appeared = {1, 3}
+
+// This does NOT work
+// Im so confused on how to do this, in C its so difficult
+// I need to code in ruby again
+int main() {
+  char str[] = "Hello World";
+
+  char words[sizeof(str) / 2][sizeof(str)];
+  int wordCounter = 0;
+
+  for (int i = 0; i < strlen(str); i++)
+  {
+    if (isalpha(str[i])) {
+      words[wordCounter][i] = str[i];
+    } else {
+      wordCounter++;
+      continue;
+    }
+  }
+
+  // printf("%li\n", sizeof(words) / sizeof(words[0]));
+
+  for (int i = 0; i < sizeof(words) / sizeof(words[0]); i++)
+  {
+    for (int x = 0; x < sizeof(words[i]) / sizeof(words[i][0]); x++)
+    {
+      printf("%c", words[i][x]);
+    }
+    printf("\n");
+  }
+}  
