5217d958a374e6d6cdbbabb6ae00c9fabd521696
od86 <178620777+od86@users.noreply.github.com>
2026-06-05 16:43:30 +0100

    Made ceaser-cipher encrypter

---
+++ ceaser-cipher/ceaser-cipher.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+  char str[] = "What a string";
+  int shifter = 5;
+  
+  for (int i = 0; i < strlen(str); i++) {
+    if (!isalpha(str[i]))
+    {
+      continue;
+    }
+
+    if (str[i] + shifter > 122) {
+      str[i] = 97 + ((str[i] + shifter) - 122);
+      continue;
+    }
+
+    if (str[i] + shifter > 90 && str[i] == toupper(str[i])) {
+      str[i] = 64 + ((str[i] + shifter) - 90);
+      continue;
+    }
+
+    str[i] = str[i] + shifter;
+  }
+
+  for (int i = 0; i < strlen(str); i++)
+  {
+    printf("%c", str[i]);
+  }
+
+  printf("\n");
+}
