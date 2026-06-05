603039ac7eb42e7f747d5decb0e44ba5a897c328
od86 <178620777+od86@users.noreply.github.com>
2026-06-05 12:43:52 +0100

    Fixed printing issue and added comment

--- bubble-sort/bubble-sort.c
+++ bubble-sort/bubble-sort.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+// Bubble sort algorithm
 int main() {
-  int unsortedArr[] = {5, 4, 3, 2, 1};
-  for (int i = 0; i < (sizeof(unsortedArr) / sizeof(unsortedArr[0])) - 1; i++)
+  int unsortedArr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+  for (int i = 0; i < (sizeof(unsortedArr) / sizeof(unsortedArr[0])); i++)
   {
-    for (int x = 0; x < (sizeof(unsortedArr) / sizeof(unsortedArr[0])) - 2; x++)
+    for (int x = 0; x < (sizeof(unsortedArr) / sizeof(unsortedArr[0])) - 1; x++)
     {
       if (unsortedArr[x] > unsortedArr[x + 1]) {
         int greater = unsortedArr[x];
@@ -13,7 +14,7 @@ int main() {
       }
     }
   }
-  for (int i = 0; i < (sizeof(unsortedArr) / sizeof(unsortedArr[0])) - 1; i++)
+  for (int i = 0; i < (sizeof(unsortedArr) / sizeof(unsortedArr[0])); i++)
   {
     printf("%i ", unsortedArr[i]);
   }
