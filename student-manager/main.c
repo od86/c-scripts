e9ca95f16a35a982ea2c51afffd04b65605947d3
od86 <178620777+od86@users.noreply.github.com>
2026-06-08 19:54:38 +0100

    More work on student manager

--- student-manager/main.c
+++ student-manager/main.c
@@ -1,83 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Functions:
-// view all students
-// create a student
-// view a student
-// edit a student
-// delete a student
-
-// maybe use a global variable to remember how many lines need to be cleared
-// printf("\e[2A\e[J");
+#include <stdbool.h>
+#include <unistd.h>
 
 typedef struct
 {
   int id;
-  char name[6];
+  char name[20];
   int age;
 } Student;
 
-void view_all_students(Student *students) {
-  printf("\e[2A\e[J");
-  printf("\nStudents:\n");
-  printf("--------------------\n");
+void add_student(Student *students) {
+  
+}
 
-  for (int i = 0; i > -1; i++)
+Student find_by_id(int id, Student *students) {
+  Student student;
+
+  for (int i = 1; i > 0; i++)
   {
-    if (students[i].id == 0) {
+    if (students[i].id > 10) {
       break;
     }
 
-    printf("ID: %i | Name: %s | Age: %i\n", 
+    if (students[i].id == id) {
+      student = students[i];
+      break;
+    }
+  }
+
+  return student;
+}
+
+void print_all_students(Student *students) {
+  printf("Students:\n");
+  printf("--------------------\n");
+  for (int i = 1; i > 0; i++)
+  {
+    if (students[i].id > 10) {
+      break;
+    }
+
+    printf("ID: %i | Name: %s | Age: %i | i: %i\n", 
       students[i].id, 
       students[i].name,
-      students[i].age);
-  }
-
-  printf("\n");
-}
-
-void determine_choice(Student *students) {
-  int choice;
-
-  printf("Enter number of what you'd like to do: ");
-  scanf("%i", &choice);
-
-  if (choice == 1) {
-    view_all_students(students);
-    determine_choice(students);
-  }
-  else {
-    printf("\n\nAhhhh thats not an option im gonna... \n");
-    printf("\n--- PROGRAM DIED ---\n\n");
+      students[i].age,
+      i);
   }
 }
 
-// Displays options and asks for input
-void print_options() {
-  printf("\n+----------------+-----+\n");
-  printf("| Function       | Num |\n");
-  printf("+----------------+-----+\n");
-  printf("| All students   |  1  |\n");
-  printf("| Create student |  2  |\n");
-  printf("| View student   |  3  |\n");
-  printf("| Edit student   |  4  |\n");
-  printf("| Delete Student |  5  |\n");
-  printf("+----------------+-----+\n\n");
-}
 
 int main() {
-  Student *students = (Student *)malloc(sizeof(Student) * 2);
+  Student *students = (Student *)malloc(sizeof(Student));
 
-  Student student1 = {1, "Jeff", 20};
-  Student student2 = {2, "Bob", 30};
+  Student student = {0, "test", 0};
+  students[0] = student;
 
-  students[0] = student1;
-  students[1] = student2;
-
-  print_options();
-  determine_choice(students);
+  add_student(students);
 
   return 0;
 }
