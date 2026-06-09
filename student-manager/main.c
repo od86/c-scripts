0490e4ecb70b200a56f366f4e7d659daf7f236ec
od86 <178620777+od86@users.noreply.github.com>
2026-06-09 20:39:17 +0100

    Scaled down project size Decided only to keep creating/viewing/deleting students Creating/Viewing individual and all students are implemented Just need to add deleting

--- student-manager/main.c
+++ student-manager/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
+
+int lines_to_clear = 1;
 
 typedef struct
 {
@@ -10,53 +13,143 @@ typedef struct
   int age;
 } Student;
 
-void add_student(Student *students) {
-  
+void show_menu() {
+  printf("\n +----------------+-----+\n");
+  printf(" | Function       | Num |\n");
+  printf(" +----------------+-----+\n");
+  printf(" | All students   |  0  |\n");
+  printf(" | Create student |  1  |\n");
+  printf(" | Find Student   |  2  |\n");
+  printf(" | Delete Student |  3  |\n");
+  printf(" | Exit program   | >=4 |\n");
+  printf(" +----------------+-----+\n\n");
 }
 
-Student find_by_id(int id, Student *students) {
-  Student student;
+void delete_student(int num_of_students, Student *students) {
 
-  for (int i = 1; i > 0; i++)
+}
+
+void find_by_id(int num_of_students, Student *students) {
+  int id;
+  bool student_exists = false;
+
+  printf(" Stduent id: ");
+  scanf("%i", &id);
+
+  printf("\n Student:\n");
+
+  for (int i = 0; i < num_of_students; i++)
   {
-    if (students[i].id > 10) {
-      break;
-    }
-
-    if (students[i].id == id) {
-      student = students[i];
+    if (students[i].id == id) 
+    {
+      printf(" Id: %i | Name: %s | Age: %i\n\n", 
+        students[i].id, 
+        students[i].name,
+        students[i].age);
+      student_exists = true;
       break;
     }
   }
+
+  if(student_exists == false) {
+    printf(" NO STUDENTS HAVE AN ID OF %i\n\n", id);
+  }
+}
+
+void show_students(int num_of_students, Student *students) {
+  printf(" STUDENTS:\n");
+
+  for (int i = 0; i < num_of_students; i++)
+  {
+    printf(" Id: %i | Name: %s | Age: %i\n", 
+      students[i].id, 
+      students[i].name,
+      students[i].age);
+  }
+
+  printf("\n");
+}
+
+Student create_student(int num_of_students) {
+  char name[20];
+  int age;
+
+  printf(" Student name: ");
+  scanf("%s", name);
+  printf(" Student age: ");
+  scanf("%i", &age);
+  printf("\n");
+
+  Student student;
+  student.id = num_of_students + 1;
+  strcpy(student.name, name);
+  student.age = age;
 
   return student;
 }
 
-void print_all_students(Student *students) {
-  printf("Students:\n");
-  printf("--------------------\n");
-  for (int i = 1; i > 0; i++)
+Student *add_student(int num_of_students, Student *students) {
+  Student student = create_student(num_of_students);
+  students = (Student *)realloc(students, (num_of_students + 1) * sizeof(Student));
+  
+  if (students == NULL) 
   {
-    if (students[i].id > 10) {
-      break;
-    }
+    printf("Memory Reallocation Failed\n");
+  }
 
-    printf("ID: %i | Name: %s | Age: %i | i: %i\n", 
-      students[i].id, 
-      students[i].name,
-      students[i].age,
-      i);
+  students[num_of_students] = student; 
+  return students;
+}
+
+void main_loop(int num_of_students, Student *students) {
+  int choice;
+
+  printf(" Enter number of what you'd like to do: ");
+  scanf("%i", &choice);
+
+  printf("\e[%iA\e[J", lines_to_clear);
+  lines_to_clear = 1;
+
+  if (choice == 0) 
+  {
+    lines_to_clear = 3 + num_of_students;
+    show_students(num_of_students, students);
+    main_loop(num_of_students, students);
+  } 
+  else if (choice == 1)
+  {
+    lines_to_clear = 4;
+    students = add_student(num_of_students, students);
+    num_of_students++;
+    main_loop(num_of_students, students);
+  } 
+  else if (choice == 2)
+  {
+    lines_to_clear = 6;
+    find_by_id(num_of_students, students);
+    main_loop(num_of_students, students);
+  }
+  else if (choice == 3)
+  {
+    lines_to_clear = 1;
+    main_loop(num_of_students, students);
+  }
+  else 
+  {
+    printf(" --- EXITING PROGRAM ---\n\n");
   }
 }
 
-
 int main() {
+  printf("\n --- STARTING PROGRAM ---\n\n");
+
+  int num_of_students = 0;
   Student *students = (Student *)malloc(sizeof(Student));
+  students = add_student(num_of_students, students);
+  num_of_students++;
 
-  Student student = {0, "test", 0};
-  students[0] = student;
-
-  add_student(students);
-
+  printf("\e[4A\e[J");
+  show_menu();
+  main_loop(num_of_students, students);
   return 0;
 }
