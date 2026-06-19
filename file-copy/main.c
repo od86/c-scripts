cbbbe4f2ef187b7e5032995367db1691927f4069
od86 <178620777+od86@users.noreply.github.com>
2026-06-19 14:23:25 +0100

    Doesnt work, not enjoying it anymore so moving onto different project

--- file-copy/main.c
+++ file-copy/main.c
@@ -3,39 +3,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc) {
-  printf("Enter name of file a: ");
-  // get file name a
-  printf("Enter name of file b: ");
-  // get file name b
+// ----- INCOMPLETE -----
+// Im so lost on this, I think ill come back to it
 
-  // open file a and b
-  FILE *file_a = fopen("a.txt", "r");
-  FILE *file_b = fopen("b.txt", "w");
+// have a look into fread/fwrite/fclose
+// errno
+
+int main() {
+  FILE *file_a = fopen("a.txt", "rb");
+  FILE *file_b = fopen("b.txt", "wb");
+
+  char buffer;
 
   if (file_a == NULL || file_b == NULL)
   {
     if (file_a == NULL)
       printf("Failed to open file a while attempting to read.\n");
-
     if (file_b == NULL)
-      printf("Failed to open file b while attempting to write.\n");
-
+      printf("Failed to open file b while attempting to write.\n");  
+    
     exit(0);
   }
 
-  while (1)
-  {
-    char c = fgetc(file_a);
-
-    if (feof(file_a))
-      break;
-
-    fputc(c, file_b);
-  }
-
-  fclose(file_a);
-  fclose(file_b);
-
-  return 0;
+  // fwrite()
 }
+
+// int main(int argc) {
+//   printf("Enter name of file a: ");
+//   // get file name a
+//   printf("Enter name of file b: ");
+//   // get file name b
+
+//   // open file a and b
+//   FILE *file_a = fopen("a.txt", "r");
+//   FILE *file_b = fopen("b.txt", "w");
+
+//   if (file_a == NULL || file_b == NULL)
+//   {
+//     if (file_a == NULL)
+//       printf("Failed to open file a while attempting to read.\n");
+
+//     if (file_b == NULL)
+//       printf("Failed to open file b while attempting to write.\n");
+
+//     exit(0);
+//   }
+
+//   while (1)
+//   {
+//     char c = fgetc(file_a);
+
+//     if (feof(file_a))
+//       break;
+
+//     fputc(c, file_b);
+//   }
+
+//   fclose(file_a);
+//   fclose(file_b);
+
+//   return 0;
+// }
