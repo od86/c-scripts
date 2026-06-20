258ea9226519592c217ffb43f2fb8667db1e7867
od86 <178620777+od86@users.noreply.github.com>
2026-06-20 10:04:17 +0100

    Finished collatz

--- recursion/main.c
+++ recursion/main.c
@@ -17,23 +17,23 @@ int fibonacci(int n) {
 }
 
 int collatz(int n) {
-  int count = 0;
-  printf("count = %i\n", count);
+  int count = 1;
+
   if (n == 1)
     return 1;
   else if (n % 2 == 0)
     return count + collatz(n / 2);
-  else if (n % 3 == 0)
+  else if (n % 2 != 0)
     return count + collatz((n * 3) + 1);
 
   return count;
 }
 
 int main() {
-  int i = 8;
+  int i = 6;
   // printf("Sum of natural nums for %i = %i\n", i, sum_of_natural_nums(i));
   // printf("Fibonacci at %i = %i\n", i, fibonacci(i));
-  printf("Collatz conjecture for %i = %i\n", i, collatz(i));
+  // printf("Collatz conjecture for %i = %i\n", i, collatz(i));
 
   return 0;
 }
